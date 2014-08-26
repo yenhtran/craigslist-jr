a69341629523b8dd9ffe82d9ca81fd773e7f8040
Anna Kalkanis & Yen Tran <pair+annakalkanis.yenhtran@devbootcamp.com>
2014-08-26 11:57:50 -0700

    Added category page with listing of associated articles

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -3,3 +3,12 @@ get '/' do
 
   erb :index
 end
+
+get '/:category' do
+  @category = Category.where(name: "#{params[:category]}")
+  @articles = []
+  @category.first.articles.each do |article|
+    @articles << article
+  end
+  erb :category
+end
